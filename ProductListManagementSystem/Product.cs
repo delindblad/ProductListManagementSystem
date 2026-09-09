c7d2553a141624db79550dd080cf8c515fd0105b
Dennis Lindblad <delindblad@gmail.com>
2026-09-09 13:01:18 +0200

    Add project files.

---
+++ ProductListManagementSystem/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductListManagementSystem
+{
+    internal class Product
+
+    {
+        string? Name { get; set; }
+        string? Category { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
